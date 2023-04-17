6f021e548d5c1d53fb2947b6056bb2d1e5d87ad5
wisecol <seoju047@naver.com>
2023-04-17 11:55:48 +0900

    first load

---
+++ AddressManagement/AddressManagement/Model/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace AddressManagement.Model
+{
+    public class Person : ReactiveObject
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { this.RaiseAndSetIfChanged(ref _name, value); }
+        }
+
+        private bool _gender;
+
+        public bool Gender
+        {
+            get { return _gender; }
+            set { this.RaiseAndSetIfChanged(ref _gender, value); }
+        }
+
+        private string _phoneNumber;
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { this.RaiseAndSetIfChanged(ref _phoneNumber, value); }
+        }
+
+        private string _address;
+       
+        public string Address
+        {
+            get { return _address; }
+            set { this.RaiseAndSetIfChanged(ref _address, value); }
+        }
+
+        public Person()
+        {
+
+        }
+
+        public Person(Person input)
+        {
+            _name = input.Name;
+            _gender = input.Gender;
+            _phoneNumber = input.PhoneNumber;
+            _address = input.Address;
+        }
+
+
+    }
+}
