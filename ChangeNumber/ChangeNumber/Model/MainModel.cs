9b12f8bb378f137cf6e88e537cdfc653bf63489f
wisecol <seoju047@naver.com>
2023-04-17 11:08:30 +0900

    training

---
+++ ChangeNumber/ChangeNumber/Model/MainModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeNumber.Model
+{
+    class MainModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private int inputNum = 1;
+        private int resNum = 1;
+
+        public int InputNum
+        {
+            get { return inputNum; }
+            set
+            {
+                inputNum = value;
+                ResNum = value * 2;
+                OnPropertyChanged("InputNum");
+            } 
+        }
+        public int ResNum
+        {
+            get { return resNum; }
+            set
+            {
+                resNum = value;
+                OnPropertyChanged("ResNum");
+            }
+        }
+    }
+}
